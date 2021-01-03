38546e4e6e4246601e94bf86fa2feb51b5952179
K Davidson <kreatord@gmail.com>
2021-01-03 20:47:11 +0000

    Update

--- onthego/lib/main.dart
+++ onthego/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_admob_app_open/flutter_admob_app_open.dart';
 import 'package:onthego/ScreenFavourites.dart';
 import 'ScreenNearby.dart';
 import 'ScreenFavourites.dart';
@@ -7,7 +8,31 @@ import 'ScreenRoutePlanner.dart';
 
 final double bottomNavigationBarHeight = 60;
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Replace your admob app ID
+  final admobAppId = "ca-app-pub-7358346462538405~1328453994";
+
+  /// Replace your admob app open ad unit id
+  final appAppOpenAdUnitId = "ca-app-pub-7358346462538405/8380129268";
+
+  MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    keywords: <String>['flutterio', 'beautiful apps'],
+    contentUrl: 'https://flutter.io',
+    birthday: DateTime.now(),
+    childDirected: false,
+    designedForFamilies: false,
+    gender: MobileAdGender.male, // or MobileAdGender.female, MobileAdGender.unknown
+    testDevices: <String>[], // Android emulators are considered test devices
+  );
+
+  await FlutterAdmobAppOpen.instance.initialize(
+    appId: admobAppId,
+    appAppOpenAdUnitId: appAppOpenAdUnitId,
+  );
+
   runApp(MyApp());
 }
 
